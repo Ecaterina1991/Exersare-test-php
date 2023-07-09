efd0ef4e91d9b960cc1f2c1f4faf1bd8a41f03f7
Ecaterina <barganecaterina@yahoo.com>
2023-07-09 16:03:49 +0300

    m

--- mysql1/index.php
+++ mysql1/index.php
@@ -6,57 +6,29 @@ $password = '';
 
 $conn = new mysqli($servername, $username, $password);
 
-if($conn->connect_error) {
-  die("Conexiune esuata" . $conn->connect_error); 
-}
-echo "Conexiune cu succes 1!";
-
-
-
-
 
 //mysqli procedural
 
-// if(!$conn) {
-//   die("Conexiune esuata".mysqli_connect_error());
-// }
-// echo "Conexiune reusita prin mysqli procedural";
+if(!$conn) {
+  die("Conexiune esuata".mysqli_connect_error());
+}
+echo "Conexiune reusita prin mysqli procedural";
 
 
 
 //Create a MySql Database 
 
-// $sql = "CREATE DATABASE mydb4";
-
-// if($conn->query($sql)===TRUE) {
-//   echo "Baza de date creata cu succes2";
-// } else {
-//   echo "Incercare esuata".$conn->error;
-// }
-// $conn->close();
-
 
 //Creare baza de date procedural
 
-// $sql = "CREATE DATABASE mydb_procedural";
-// if(mysqli_query($conn, $sql)) {
-//   echo "Baza de date creata cu succes - procedural";
-// } else {
-//   echo "Eroare" . mysqli_error($conn);
-// }
-// mysqli_close($conn);
-
-//PDO
-
-try{
-  $conn = new PDO("mysql:host=$servername", $username, $password);
-  //set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "CREATE DATABASE myDb101";
-  //use exec()  bacause no results are returned
-  $conn->exec($sql);
-
+$sql = "CREATE DATABASE mydb_procedural";
+if(mysqli_query($conn, $sql)) {
+  echo "Baza de date creata cu succes - procedural";
+} else {
+  echo "Eroare" . mysqli_error($conn);
 }
-//https://www.w3schools.com/php/php_mysql_create.asp
+mysqli_close($conn);
+
+
 
 ?>
